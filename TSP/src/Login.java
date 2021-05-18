0e90c225eaf164d794ca53dca5a3ddc9f23b100d
Isaac Haro <74152544+ieharo@users.noreply.github.com>
2021-05-18 08:07:29 -0500

    Revert "alan prueba 3"
    This reverts commit 62288b66b1835822af826b9bbb53a60d3b3d6717.

--- TSP/src/Login.java
+++ TSP/src/Login.java
@@ -141,8 +141,6 @@ public class Login extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
     //JALSKASALKSALASKSAJDASLKDASLFHSALFHASL
     /**
-     * sadsadsadsadsa
-     * sadasd
      * @param args the command line arguments
      */
     public static void main(String args[]) {
