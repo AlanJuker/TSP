728ec87bb2a2afa24c7764a665333d3a6705c373
AlanJuker <alan4630@gmail.com>
2021-05-29 18:30:34 -0500

    a

--- TSP/src/DP/Interface/IPropertiesDB.java
+++ TSP/src/DP/Interface/IPropertiesDB.java
@@ -15,5 +15,52 @@ public interface IPropertiesDB {
     String getRaiz();
     String getConexion();
     String getDB();
+            
+    //CRUD alimento
+    String getInsertAlimento();
+    String getModificarAlimento();
+    String getConsultarAlimento();
+    String getEliminarAlimento();
+    
+    //CRUD citamedica
+    String getInsertCitaMedica();
+    String getModificarCitaMedica();
+    String getConsultarCitaMedica();
+    String getEliminarCitaMedica();
+    
+    //CRUD ejercicio
+    String getInsertEjercicio();
+    String getModificarEjercicio();
+    String getConsultarEjercicio();
+    String getEliminarEjercicio();
+    
+    //CRUD imc
+    String getInsertIMC();
+    String getModificarIMC();
+    String getConsultarIMC();
+    String getEliminarIMC();
+    
+    //CRUD medico
+    String getInsertMedico();
+    String getModificarMedico();
+    String getConsultarMedico();
+    String getEliminarMedico();
+    
+    //CRUD paciente
     String getInsertPaciente();
+    String getModificarPaciente();
+    String getConsultarPaciente();
+    String getEliminarPaciente();
+    
+    //CRUD plannutricion
+    String getInsertPlanNutricion();
+    String getModificarPlanNutricion();
+    String getConsultarPlanNutricion();
+    String getEliminarPlanNutricion();
+    
+    //CRUD rutina
+    String getInsertRutina();
+    String getModificarRutina();
+    String getConsultarRutina();
+    String getEliminarRutina();   
 }
--- TSP/src/DP/Interface/PropertiesDB.java
+++ TSP/src/DP/Interface/PropertiesDB.java
@@ -32,20 +32,160 @@ public class PropertiesDB implements IPropertiesDB {
     public String getRaiz() {
         return System.getProperty("user.dir");
     }
-
     @Override
     public String getConexion() {
         return _properties.getProperty("conexion");
     }
-
     @Override
     public String getDB() {
         return _properties.getProperty("db");
     }
 
+    //CRUD alimento
     @Override
-    public String getInsertPaciente(){
-        return _properties.getProperty("insertPaciente");
+    public String getInsertAlimento() {
+        return _properties.getProperty("insertAlimento");
+    }
+    @Override
+    public String getModificarAlimento() {
+        return _properties.getProperty("modificarAlimento");
+    }
+    @Override
+    public String getConsultarAlimento() {
+        return _properties.getProperty("consultarAlimento");
+    }
+    @Override
+    public String getEliminarAlimento() {
+        return _properties.getProperty("eliminarAlimento");
     }
 
+    //CRUD citamedica
+    @Override
+    public String getInsertCitaMedica() {
+        return _properties.getProperty("insertarCitaMedica");
+    }
+    @Override
+    public String getModificarCitaMedica() {
+        return _properties.getProperty("modificarCitaMedica");
+    }
+    @Override
+    public String getConsultarCitaMedica() {
+        return _properties.getProperty("consultarCitaMedica");
+    }
+    @Override
+    public String getEliminarCitaMedica() {
+        return _properties.getProperty("eliminarCitaMedica");
+    }
+    
+    //CRUD ejercicio
+    @Override
+    public String getInsertEjercicio() {
+        return _properties.getProperty("insertEjercicio");
+    }
+    @Override
+    public String getModificarEjercicio() {
+        return _properties.getProperty("modificarEjercicio");
+    }
+    @Override
+    public String getConsultarEjercicio() {
+        return _properties.getProperty("consultarEjercicio");
+    }
+    @Override
+    public String getEliminarEjercicio() {
+        return _properties.getProperty("eliminarEjercicio");
+    }
+    
+    //CRUD imc
+    @Override
+    public String getInsertIMC() {
+        return _properties.getProperty("insertarEjercicio");
+    }
+    @Override
+    public String getModificarIMC() {
+        return _properties.getProperty("modificarEjercicio");
+    }
+    @Override
+    public String getConsultarIMC() {
+        return _properties.getProperty("consultarIMC");
+    }
+    @Override
+    public String getEliminarIMC() {
+        return _properties.getProperty("eliminarIMC");
+    }
+
+    //CRUD medica
+    
+    @Override
+    public String getInsertMedico() {
+        return _properties.getProperty("insertarMedico");
+    }
+    @Override
+    public String getModificarMedico() {
+        return _properties.getProperty("modificarMedico");
+    }
+    @Override
+    public String getConsultarMedico() {
+        return _properties.getProperty("consultarMedico");
+    }
+    @Override
+    public String getEliminarMedico() {
+        return _properties.getProperty("eliminarMedico");
+    }
+    
+    //CRUD paciente
+
+    @Override
+    public String getInsertPaciente() {
+        return _properties.getProperty("insertPaciente");
+    }
+    @Override
+    public String getModificarPaciente() {
+        return _properties.getProperty("modificarPaciente");
+    }
+    @Override
+    public String getConsultarPaciente() {
+        return _properties.getProperty("consultarPaciente");
+    }
+    @Override
+    public String getEliminarPaciente() {
+        return _properties.getProperty("eliminarPaciente");
+    }
+    
+    //CRUD plannutricion
+
+    @Override
+    public String getInsertPlanNutricion() {
+        return _properties.getProperty("insertarPaciente");
+    }
+    @Override
+    public String getModificarPlanNutricion() {
+        return _properties.getProperty("modificarPaciente");
+    }
+    @Override
+    public String getConsultarPlanNutricion() {
+        return _properties.getProperty("consultarPlanNutricion");
+    }
+    @Override
+    public String getEliminarPlanNutricion() {
+        return _properties.getProperty("eliminarPlanNutricion");
+    }
+
+    //CRUD rutina
+    
+    @Override
+    public String getInsertRutina() {
+        return _properties.getProperty("insertRutina");
+    }
+    @Override
+    public String getModificarRutina() {
+        return _properties.getProperty("modificarRutina");
+    }
+    @Override
+    public String getConsultarRutina() {
+        return _properties.getProperty("consultarRutina");
+    }
+    @Override
+    public String getEliminarRutina() {
+        return _properties.getProperty("eliminarRutina");
+    }
 }
